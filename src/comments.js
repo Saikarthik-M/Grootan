87e2396de5bf10e0f06c9f49240fb39f0c3b936f
saikarthik <68589580+Saikarthik-M@users.noreply.github.com>
2021-05-01 11:25:47 +0530

    Update comments.js

--- src/comments.js
+++ src/comments.js
@@ -25,10 +25,10 @@ class Comments extends React.Component {
     }
     render() {
         return (
-            <div class="btn w-100 text-dark my-3" style={{ backgroundColor: "orange", pointerEvents: "none" }}>
-                <div class="row my-0 py-0">
-                    <h6 class="ml-5 mr-auto my-0 py-0 text-white">{this.state.by}</h6>
-                    <small class="mr-3 text-secondary">{this.state.date}</small>
+            <div className="btn w-100 text-dark my-3" style={{ backgroundColor: "orange", pointerEvents: "none" }}>
+                <div className="row my-0 py-0">
+                    <h6 className="ml-5 mr-auto my-0 py-0 text-white">{this.state.by}</h6>
+                    <small className="mr-3 text-secondary">{this.state.date}</small>
                 </div>
                 <hr />
                 <p style={{ overflow: "hidden" }}>{this.state.comment}</p>
